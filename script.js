2f5c1ba7323794f4cee7327be91b2380aae08cac
mamismeda <amamismeda@gmail.com>
2022-01-15 19:20:16 +0400

    increment decrement buttons

---
+++ script.js
@@ -0,0 +1,23 @@
+let incrementButton = document.querySelector(".increment");
+let decrementButton = document.querySelector(".decrement");
+let inputArea = document.querySelector(".inpy");
+let count = 0;
+
+inputArea.innerHTML = count;
+
+const fixIncrement = (el) => {
+
+    count ++ ;
+    inputArea.innerHTML = count;
+    
+};
+
+const fixDecrement = () => {
+
+    count -- ;
+    inputArea.innerHTML = count;
+};
+
+incrementButton.addEventListener("click", fixIncrement);
+decrementButton.addEventListener("click", fixDecrement);
+
